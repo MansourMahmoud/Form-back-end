30f3239f5b32c4c55b0ae8bdcb5dd91ceb450e9a
MansourMahmoud <mansourmahmoud77a@gmail.com>
2024-02-08 22:23:35 +0200

    fix

--- index.js
+++ index.js
@@ -13,18 +13,117 @@ const upload = require("./middleware/uploadFile");
 const { ERROR } = require("./utils/httpStatusText");
 
 app.post("/send", upload.array("images", 8), (request, response) => {
-  const {
+  let {
     fullName,
     phone,
     email,
     products,
-    numberOfProducts,
+    services,
+    newService,
     region,
     city,
     locationDetails,
   } = request.body;
 
+  if (products) {
+    if (typeof products === "string") {
+      let productsUpdated = [];
+      productsUpdated.push(products);
+      products = productsUpdated.map((product) => JSON.parse(product));
+    } else if (Array.isArray(products)) {
+      const parsedProducts = products.map((product) => JSON.parse(product));
+      products = parsedProducts;
+    }
+  }
+  if (services) {
+    if (typeof services === "string") {
+      let servicesUpdated = [];
+      servicesUpdated.push(services);
+      services = servicesUpdated.map((service) => JSON.parse(service));
+    } else if (Array.isArray(services)) {
+      parsedServices = services.map((service) => JSON.parse(service));
+      services = parsedServices;
+    }
+  }
+
   const files = request.files;
+
+  const isServicesAndNewService =
+    services &&
+    newService &&
+    `
+  <div style="display: flex; gap: 10px;">
+    <span style="font-size: 16px; color: black;"> نوع البناء الذي اختاره المستخدم :- </span>
+  </div>
+  <div style="overflow-x: auto;">
+    <table style="border-collapse: collapse; width: 100%; font-size: 18px;">
+      <thead>
+        <tr>
+          <th style="border: 1px solid #ddd; padding: 8px; text-align: center; color: black;">
+            نوع البناء
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        ${services
+          ?.map(
+            (service) => `
+      <tr>
+        <td style="border: 1px solid #ddd; padding: 8px; color: green;">${service.name}</td>
+      </tr>
+    `
+          )
+          .join("")}
+      </tbody>
+    </table>
+  </div>
+
+  <div style="display: flex; gap: 10px;">
+    <span style="font-size: 16px; color: black;"> لقد أضاف المستخدم نوع بناء جديد وهو :- </span>
+    <span style="font-size: 16px; color: green; font-weight: 600;">${newService}</span>
+  </div>
+`;
+
+  const isServicesOnly =
+    services &&
+    !newService &&
+    `
+<div style="display: flex; gap: 10px;">
+  <span style="font-size: 16px; color: black;"> نوع البناء الذي اختاره المستخدم :- </span>
+</div>
+<div style="overflow-x: auto;">
+  <table style="border-collapse: collapse; width: 100%; font-size: 18px;">
+    <thead>
+      <tr>
+        <th style="border: 1px solid #ddd; padding: 8px; text-align: center; color: black;">
+          نوع البناء
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      ${services
+        ?.map(
+          (service) => `
+    <tr>
+      <td style="border: 1px solid #ddd; padding: 8px; color: green;">${service.name}</td>
+    </tr>
+  `
+        )
+        .join("")}
+    </tbody>
+  </table>
+</div>
+`;
+
+  const isNewServiceOnly =
+    !services &&
+    newService &&
+    `
+<div style="display: flex; gap: 10px;">
+  <span style="font-size: 16px; color: black;"> لقد أضاف المستخدم نوع بناء جديد ولم يختر نوع بناء من المقترح وهو :- </span>
+  <span style="font-size: 16px; color: green; font-weight: 600;"> ${newService} </span>
+</div>
+`;
 
   const transporter = nodemailer.createTransport({
     service: "Gmail",
@@ -33,59 +132,82 @@ app.post("/send", upload.array("images", 8), (request, response) => {
       pass: "twgzuhqfivzgtkqv",
     },
   });
-
+  // alkaseralamni@gmail.com
   const mail_option = {
     from: request.body.email || "alkaser@gmail.com",
-    to: "alkaseralamni@gmail.com",
+    to: "mansourmahmoud77a@gmail.com",
     subject: "عرض اسعار",
     text: "عرض اسعار", // plain text body
     html: `
       <div  dir="rtl">
 
           <div style="display: flex; gap: 10px;">
-            <span style="font-size: 22px; color: black;"> الإسم :- </span>
-            <span style="font-size: 22px; color: green; font-weight: 600;">${fullName}</span>
+            <span style="font-size: 16px; color: black;"> الإسم :- </span>
+            <span style="font-size: 16px; color: green; font-weight: 600;">${fullName}</span>
            </div>
 
           <div style="display: flex; gap: 10px;">
-            <span style="font-size: 22px; color: black;"> رقم الجوال :- </span>
-            <span style="font-size: 22px; color: green; font-weight: 600;">${phone}</span>
+            <span style="font-size: 16px; color: black;"> رقم الجوال :- </span>
+            <span style="font-size: 16px; color: green; font-weight: 600;">${phone}</span>
           </div>
 
         <div style="display: flex; gap: 10px;">
-          <span style="font-size: 22px; color: black;"> الإيميل  :- </span>
-          <span style="font-size: 22px; color: green; font-weight: 600;">${email}</span>
+          <span style="font-size: 16px; color: black;"> الإيميل  :- </span>
+          <span style="font-size: 16px; color: green; font-weight: 600;">${email}</span>
         </div>
 
         <div style="display: flex; gap: 10px;">
-          <span style="font-size: 22px; color: black;"> المنتجات التي اختارها المستخدم  :- </span>
-          <span style="font-size: 22px; color: green; font-weight: 600;">${products}</span>
+          <span style="font-size: 16px; color: black;"> المنتجات التي اختارها المستخدم  :- </span>
+        </div>
+        <div style="overflow-x: auto;">
+        <table style="border-collapse: collapse; width: 100%; font-size: 18px;">
+          <thead>
+            <tr>
+              <th style="border: 1px solid #ddd; padding: 8px; text-align: center; color: black;">المنتج</th>
+              <th style="border: 1px solid #ddd; padding: 8px; text-align: center; color: black;">العدد</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${products
+              .map(
+                (product) => `
+              <tr>
+                <td style="border: 1px solid #ddd; padding: 8px; color: green;">${product.productName}</td>
+                <td style="border: 1px solid #ddd; padding: 8px; text-align: center; color: green;">${product.num}</td>
+              </tr>
+            `
+              )
+              .join("")}
+          </tbody>
+        </table>
+      </div>
+
+      ${isServicesAndNewService ? isServicesAndNewService : `<span></span>`}
+
+      ${isServicesOnly ? isServicesOnly : `<span></span>`}
+
+      ${isNewServiceOnly ? isNewServiceOnly : `<span></span>`}
+
+
+        <div style="display: flex; gap: 10px;">
+          <span style="font-size: 16px; color: black;"> المنطقة :- </span>
+          <span style="font-size: 16px; color: green; font-weight: 600;">${region}</span>
         </div>
 
         <div style="display: flex; gap: 10px;">
-          <span style="font-size: 22px; color: black;"> وعددها :- </span>
-          <span style="font-size: 22px; color: green; font-weight: 600;">${numberOfProducts}</span>
+          <span style="font-size: 16px; color: black;"> المدينة :- </span>
+          <span style="font-size: 16px; color: green; font-weight: 600;">${city}</span>
         </div>
 
         <div style="display: flex; gap: 10px;">
-          <span style="font-size: 22px; color: black;"> المنطقة :- </span>
-          <span style="font-size: 22px; color: green; font-weight: 600;">${region}</span>
-        </div>
-
-        <div style="display: flex; gap: 10px;">
-          <span style="font-size: 22px; color: black;"> المدينة :- </span>
-          <span style="font-size: 22px; color: green; font-weight: 600;">${city}</span>
-        </div>
-
-        <div style="display: flex; gap: 10px;">
-          <span style="font-size: 22px; color: black;"> تفاصيل العنوان (اللوكيشن) :- </span>
-          <span style="font-size: 22px; color: green; font-weight: 600;">${locationDetails}</span>
+          <span style="font-size: 16px; color: black;"> تفاصيل العنوان (اللوكيشن) :- </span>
+          <span style="font-size: 16px; color: green; font-weight: 600;">${locationDetails}</span>
         </div>
 
       </div>
 
       `,
-    attachments: files.map((file, index) => ({
+    attachments: files?.map((file, index) => ({
       filename: `image_${index + 1}.jpg`, // اسم الملف المرفوع
       content: file.buffer, // المحتوى الخاص بالملف
       encoding: "base64", // الترميز
